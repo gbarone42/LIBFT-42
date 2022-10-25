3915296884e06c9c4af85cb9a2e2cf78c0a0d1f1
Giuseppe Barone <gbarone@c1r4p3.42firenze.it>
2022-10-25 15:06:54 +0200

    oct25/15::00

---
+++ libft/ft_lstnew.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gbarone <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/25 13:38:35 by gbarone           #+#    #+#             */
+/*   Updated: 2022/10/25 13:45:10 by gbarone          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{	
+	t_list	*node;
+
+	node = (t_list *)malloc(sizeof(t_list));
+	node->content = content;
+	node->next = NULL;
+	return (nodo);
+}

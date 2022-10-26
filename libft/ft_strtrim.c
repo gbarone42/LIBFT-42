df5561f006a002c5c831b71e0c39cdaeed7dd862
Giuseppe Barone <gbarone@student.42firenze.it>
2022-10-26 19:31:42 +0200

    okokok

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -84,80 +84,52 @@ char	*ft_strtrim(char const *s1, char const *set)
 	trim[k] = '\0';
 	return (trim);
 }
-/* questa e'quella di prima
-size_t	check_first(char const *s1, char const *set)
+/*
+static int	ft_inset(const char c, char const *set)
 {
-	size_t	res;
-	size_t	i;
-	size_t	j;
+	int	i;
 
-	res = 0;
 	i = 0;
-	j = 0;
-	while (set[j])
+	while (set[i] != 0)
 	{
-		if (s1[i] == set[j])
-		{
-			res++;
-			j = 0;
-			i++;
-		}
-		else
-			j++;
+		if (c == set[i])
+			return (1);
+		i++;
 	}
-	return (res);
-}
-
-size_t	check_last(char const *s1, char const *set)
-{
-	size_t	res;
-	size_t	i;
-	size_t	j;
-
-	res = ft_strlen(s1);
-	i = ft_strlen(s1) - 1;
-	j = 0;
-	while (set[j])
-	{
-		if (s1[i] == set[j])
-		{
-			res--;
-			j = 0;
-			i--;
-		}
-		else
-			j++;
-	}
-	return (res);
+	return (0);
 }
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	char	*trims;
-	size_t	first;
-	size_t	last;
-	size_t	i;
+	char	*p;
+	int		l;
+	int		i;
+	int		s;
+	int		e;
 
+	l = ft_strlen(s1);
 	i = 0;
-	first = check_first(s1, set);
-	last = check_last(s1, set);
-	trims = (char *)malloc(last - first + 1);
-	if (!trims)
-		return (NULL);
-	while (first < last)
-	{
-		trims[i] = s1[first];
+	while (i < l && ft_inset(s1[i], set))
 		i++;
-		first++;
-	}
-	trims[i] = '\0';
-	return (trims);
+	if (i == l)
+		return (ft_calloc(1, 1));
+	s = i;
+	i = l - 1;
+	while (i >= 0 && ft_inset(s1[i], set))
+		i--;
+	e = i;
+	p = ft_calloc(l - s - (l - e) + 2, 1);
+	if (!p)
+		return (0);
+	i = -1;
+	while (s + (++i) <= e)
+		p[i] = s1[s + i];
+	return (p);
 }
-*/
-/*		
-**int                    main(void)
-**{
-**    ft_putendl_fd(ft_strtrim("lorem ipsum dolor sit am", "oa"), 1);
-**    return (0);
-**}
-*/
+
+int	main()
+{
+	const char s[] = "12915231uoh";
+	const char set[] = "abc";
+	printf("%s\n", ft_strtrim(s, set));
+} */
